3f52c209f83da6fc25eb207e719f58355ec5ce55
Alex Roberts <alex@redprocess.com>
2011-01-09 23:23:03 -0800

    New Auth Window now appears on command! Hooray!

--- NewAuthWindowController.h
+++ NewAuthWindowController.h
@@ -12,10 +12,15 @@
 @interface NewAuthWindowController : NSWindowController {
 	NSManagedObjectContext *managedObjectContext;
 	NSManagedObjectContext *parentManagedObjectContext;
-	NSObjectController *account;
-	NSArrayController *accountsArray;
+	NSObjectController *accountController;
+	NSArrayController *accountsArrayController;
 
 }
 @property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) IBOutlet NSManagedObjectContext *parentManagedObjectContext;
 
+@property (nonatomic, retain) IBOutlet NSArrayController *accountsArrayController;
+@property (nonatomic, retain) IBOutlet NSObjectController *accountController;
+
+- (IBAction)add:(id)sender;
 @end
--- NewAuthWindowController.m
+++ NewAuthWindowController.m
@@ -11,6 +11,35 @@
 #import "constants.h"
 
 @implementation NewAuthWindowController
-@synthesize managedObjectContext;
+@synthesize managedObjectContext, parentManagedObjectContext, accountsArrayController, accountController;
+
+- (NSManagedObjectContext *)parentManagedObjectContext {
+	return [self.accountsArrayController managedObjectContext];
+}
+
+- (NSManagedObjectContext *)managedObjectContext {
+	if (managedObjectContext == nil) {
+		managedObjectContext = [[NSManagedObjectContext alloc] init];
+		[managedObjectContext setPersistentStoreCoordinator:[self.parentManagedObjectContext persistentStoreCoordinator]];
+	}
+	
+	return managedObjectContext;
+}
+
+- (IBAction)add:(id)sender {
+	if (self.window == nil) {
+		NSNib *nib = [[NSNib alloc] initWithNibNamed:@"NewAuthWindow" bundle:[NSBundle bundleForClass:[self class]]];
+		BOOL fail = [nib instantiateNibWithOwner:self topLevelObjects:nil];
+		
+		if (fail != YES) {
+			DLog(@"Error instantiating sheet");
+			return;
+		}
+	}
+	
+	[self.window center];
+	[self.window makeKeyAndOrderFront:nil];
+}
+
 
 @end
